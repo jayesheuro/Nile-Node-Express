abd22962d445100f25142b788cbe13bb9a303d7f
aditya-dawadikar <adityadawadikar2000@gmail.com>
2022-02-27 21:36:20 +0530

    Create delete and get api created for products

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ dotenv.config()
 
 const User = require("./configs");
 const firebase = require("firebase");
+const mongoose = require('mongoose')
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+
+const url = process.env.MONGODB_CONNECTION_STRING
+mongoose
+    .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log("Database connected!"))
+    .catch(err => console.log(err));
+
 
 const loginRoute = require('./app/routes/entry/login');
 const registerRoute = require('./app/routes/entry/register');
@@ -24,10 +36,10 @@ app.use('/api/login', loginRoute);
 app.use('/api/register', registerRoute);
 app.use('/api/sellers', sellerRoutes);
 app.use('/api/detail/', UsersRoutes)
-app.use('/api/products/', ProductsRoutes)
+app.use('/api/products', ProductsRoutes)
 app.use('/api/cart/', CartRoutes)
 app.use('/api/watchlist/', WatchlistRoutes)
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log(`listening running on ${process.env.PORT}`)
 })
