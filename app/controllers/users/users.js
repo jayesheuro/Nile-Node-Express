b40a144c0f02805765e13308d3a3cd40cbbee6b1
Jatin Sadhwani <Jatinsadhwani.1234@gmail.com>
2022-03-09 13:27:23 +0530

    Update apis

--- app/controllers/users/users.js
+++ app/controllers/users/users.js
@@ -54,7 +54,7 @@ module.exports.getUsersInfo = async (req, res) => {
     snapshot.forEach(doc => {
         data.push({ Address: doc.data().Address, userId: doc.data().userId, Contact: doc.data().Contact })
         userId = doc.data().userId
-
+        // console.log(data[0].Contact.username)
         res.status(200).json({
             status: data
         })
