1741dd344a2b107d927930e7a424a16837afdd91
aditya-dawadikar <adityadawadikar2000@gmail.com>
2022-03-10 13:59:35 +0530

    updated api for cart

--- app/controllers/entry/register.js
+++ app/controllers/entry/register.js
@@ -12,8 +12,11 @@ module.exports.CustomerRegister = async (req, res) => {
         userId: userCredential.user.uid,
         isSeller :false,
         Address: [],
-        other_product_info: '',
-        product_selected: [],
+        cart:{
+          total_products:0,
+          product_selected:[],
+          total_amount:0 
+        },
         watch_list: [],
         Payment_method: [],
         transaction_history: [],
