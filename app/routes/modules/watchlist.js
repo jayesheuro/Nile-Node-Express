c632e8290c3c0ba610f971390d59b01fe6b67904
Jayesh Singh <jayeshsingh001@gmail.com>
2022-03-10 14:55:03 +0530

    made get to post in GET WATCHLIST API

--- app/routes/modules/watchlist.js
+++ app/routes/modules/watchlist.js
@@ -1,15 +1,31 @@
-const express = require('express');
-var bodyParser = require('body-parser')
+const express = require("express");
+var bodyParser = require("body-parser");
 const router = express.Router();
 
-const WatchlistRoutes = require('../../controllers/modules/watchlist');
+const WatchlistRoutes = require("../../controllers/modules/watchlist");
 router.use(bodyParser.json());
 
-const auth = require('../../services/auth/firebaseAuth')
+const auth = require("../../services/auth/firebaseAuth");
 
-router.post('/watchlists',auth.checkAuthWithFirebase, WatchlistRoutes.userWatchlist);
-router.get('/',auth.checkAuthWithFirebase, WatchlistRoutes.displayUserWatchlist);
-router.put('/update/:ind',auth.checkAuthWithFirebase, WatchlistRoutes.updateUserWatchlist);
-router.delete('/delete/:product_id',auth.checkAuthWithFirebase, WatchlistRoutes.deleteUserWatchlist);
+router.post(
+  "/watchlists",
+  auth.checkAuthWithFirebase,
+  WatchlistRoutes.userWatchlist
+);
+router.post(
+  "/",
+  auth.checkAuthWithFirebase,
+  WatchlistRoutes.displayUserWatchlist
+);
+router.put(
+  "/update/:ind",
+  auth.checkAuthWithFirebase,
+  WatchlistRoutes.updateUserWatchlist
+);
+router.delete(
+  "/delete/:product_id",
+  auth.checkAuthWithFirebase,
+  WatchlistRoutes.deleteUserWatchlist
+);
 
 module.exports = router;
