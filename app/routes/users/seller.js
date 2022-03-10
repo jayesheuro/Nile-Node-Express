69d11db5745a6b41ba4df5c8dc69503d10cdaed7
Jayesh Singh <jayeshsingh001@gmail.com>
2022-03-11 01:13:16 +0530

    turned-getSeller into PostSeller api

--- app/routes/users/seller.js
+++ app/routes/users/seller.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const sellerRoutes = require('../../controllers/users/seller');
+const sellerRoutes = require("../../controllers/users/seller");
 
-const auth = require('../../services/auth/firebaseAuth')
+const auth = require("../../services/auth/firebaseAuth");
 
-router.post('/seller', auth.checkAuthWithFirebase, sellerRoutes.addNewSeller);
-router.get('/', auth.checkAuthWithFirebase, sellerRoutes.displaySellerInfo);
+router.post("/seller", auth.checkAuthWithFirebase, sellerRoutes.addNewSeller);
+router.post("/", auth.checkAuthWithFirebase, sellerRoutes.displaySellerInfo);
 // router.put('/update', auth.checkAuthWithFirebase, sellerRoutes.updateSellerInfo);
 
 module.exports = router;
